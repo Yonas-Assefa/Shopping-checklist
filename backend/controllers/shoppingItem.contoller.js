3becde169bce028488ff531a7d29ed99a313941c
Yonas Assefa <se.yonas.assefa@gmail.com>
2023-08-22 10:49:44 +0300

    shopping item CRUD done

--- backend/controllers/shoppingItem.contoller.js
+++ backend/controllers/shoppingItem.contoller.js
@@ -14,3 +14,61 @@ exports.getShoppingItems = asyncHandler(async (req, res, next) => {
     data: shoppingItems,
   });
 });
+
+//@desc Get single shooping items
+//@route GET api/v1/shoppingItems/:id
+//@acces public
+exports.getShoppingItem = asyncHandler(async (req, res, next) => {
+  const shoppingItem = await ShoppingItem.findById(req.params.id);
+  if (!shoppingItem) {
+    return next(
+      new ErrorResponse(`shoppingItem not found on id ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: shoppingItem });
+});
+
+//@desc Create new shoppingItem
+//@route POST api/v1/shoppingItems
+//@acces private
+exports.createShoppingItem = asyncHandler(async (req, res, next) => {
+  const shoppingItem = await ShoppingItem.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: shoppingItem,
+  });
+});
+
+//@desc Update  shoppingItem
+//@route PUT api/v1/shoppingItems/:id
+//@acces private
+exports.updateShoppingItem = asyncHandler(async (req, res, next) => {
+  const shoppingItem = await ShoppingItem.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(201).json({ success: true, data: bootcamp });
+
+  if (!shoppingItem) {
+    return next(
+      new ErrorResponse(`shoppingItem not found on id ${req.params.id}`, 404)
+    );
+  }
+});
+
+//@desc Delete  shoppingItem
+//@route Delte api/v1/shoppingItems/:id
+//@acces private
+exports.deleteShoppingItem = asyncHandler(async (req, res, next) => {
+  const shoppingItem = await ShoppingItem.findByIdAndDelete(req.params.id);
+  res.status(200).json({ success: true, data: {} });
+  if (!shoppingItem) {
+    return next(
+      new ErrorResponse(`shoppingItem not found on id ${req.params.id}`, 404)
+    );
+  }
+});
--- backend/routes/shoppingItems.route.js
+++ backend/routes/shoppingItems.route.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getShoppingItems } = require("../controllers/shoppingItem.contoller");
+const {
+  getShoppingItems,
+  getShoppingItem,
+  createShoppingItem,
+  updateShoppingItem,
+  deleteShoppingItem,
+} = require("../controllers/shoppingItem.contoller");
 
-router.route("/").get(getShoppingItems);
+router.route("/").get(getShoppingItems).post(createShoppingItem);
+router
+  .route("/:id")
+  .get(getShoppingItem)
+  .put(updateShoppingItem)
+  .delete(deleteShoppingItem);
 
 module.exports = router;
